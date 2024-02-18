8c2b83bb908575584c9fde2dee32c876b7548e50
sbcshop3 <129948335+sbcshop3@users.noreply.github.com>
2024-02-18 20:41:06 +0530

    Update display_image.py

--- examples/display_image.py
+++ examples/display_image.py
@@ -7,10 +7,9 @@ e_paper = epd.E_paper()
 e_paper.imageblack.fill(0xff)
 e_paper.imagered.fill(0x00)
     
-e_paper.imagered.blit(pics.sb_logo , 100,50) # print sb components logo
+e_paper.imagered.blit(pics.sb_logo , 0,0) # print sb components logo
 e_paper.display(e_paper.buffer_black,e_paper.buffer_red)
 
-e_paper.clear_screen()
 print("sleep")
 e_paper.sleep()
 
